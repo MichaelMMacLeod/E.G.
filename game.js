04cd1b3cfeb0914d225229d5f45844708f13dfb1
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-03-19 12:42:50 -0700

    Make planet rotatable

--- game.js
+++ game.js
@@ -1,15 +1,15 @@
 var neptune;
 
 function startGame() {
-	neptune = new imageComponent('planet.png', 0, 0, 800, 800, 0);
+	neptune = new imageComponent('planet.png', 500, 500, 800, 800, 10);
 	gameArea.start();
 }
 
 var gameArea = {
 	canvas : canvas = document.createElement("canvas"),
 	start : function() {
-		this.canvas.width = 960;
-		this.canvas.height = 540;
+		this.canvas.width = 1000;
+		this.canvas.height = 1000;
 		this.context = this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 		this.interval = setInterval(updateGameArea, 20);
@@ -33,11 +33,11 @@ function imageComponent(source, x, y, width, height, rotation) {
 	this.rotation = rotation;
 	this.update = function() {
 		ctx = gameArea.context;
-		ctx.save();
-		ctx.translate(gameArea.width/2, gameArea.height/2);
-		ctx.rotate(this.rotation*Math.PI/180);
-		ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-		ctx.restore();
+		ctx.translate(500, 500);
+		ctx.rotate(this.rotation);
+		ctx.drawImage(this.image, -this.width / 2, -this.height / 2, this.width, this.height);
+		ctx.rotate(this.rotation);
+		ctx.translate(-500, -500);
 	}
 }
 
