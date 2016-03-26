acf80912dace5cf862ab5467c2f73dd4ab247553
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-03-26 10:28:52 -0700

    Increase canvas size.

--- game.js
+++ game.js
@@ -11,8 +11,8 @@ config = {
 gameArea = {
 	canvas : document.createElement('canvas'),
 	start : function() {
-		this.canvas.width = 4800;
-		this.canvas.height = 2700;
+		this.canvas.width = 960;
+		this.canvas.height = 540;
 		this.context = this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 		this.interval = setInterval(updateGameArea, config.fps);
