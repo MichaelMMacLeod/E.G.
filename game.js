457f5c3ad730a75a68193ddbaf6a5abbfd4c0f4a
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-03-26 09:00:26 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,43 +1,59 @@
-var neptune;
-
-function startGame() {
-	neptune = new imageComponent('planet.png', 500, 1000, 800, 800, 0);
+startGame = function() {
 	gameArea.start();
+	neptune = new component(200, 200, 240, 135, 20, "planet.png");
 }
 
-var gameArea = {
-	canvas : canvas = document.createElement("canvas"),
+gameArea = {
+	canvas : document.createElement('canvas'),
 	start : function() {
-		this.canvas.width = 1000;
-		this.canvas.height = 1000;
+		this.canvas.width = 4800;
+		this.canvas.height = 2700;
 		this.context = this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 		this.interval = setInterval(updateGameArea, 20);
 	},
 	clear : function() {
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-	},
-	stop : function() {
-		clearInterval(this.interval);
 	}
 }
 
-function imageComponent(source, x, y, width, height, rotation) {
-	this.source = source;
-	this.x = x;
-	this.y = y;
+function component(width, height, x, y, rotation, source) {
 	this.image = new Image();
 	this.image.src = source;
 	this.width = width;
 	this.height = height;
-	this.rotation = rotation;
+	this.x = x - this.width / 2;
+	this.y = y - this.height / 2;
+	this.rotation = rotation * Math.PI / 180;
+	this.ab = [ // The bottom right corner of a shape at 0 degrees rotation
+		this.width + this.x,
+		this.height + this.y
+	];
+	this.midpoint = [ // The midpoint of a shape at 0 degrees rotation
+		(this.x + this.ab[0]) / 2, 
+		(this.y + this.ab[1]) / 2
+	];
+	this.abNew = [ // The bottom right corner of the rotated shape
+		this.ab[1] * Math.sin(-this.rotation) + this.ab[0] * Math.cos(-this.rotation),
+		this.ab[1] * Math.cos(-this.rotation) - this.ab[0] * Math.sin(-this.rotation)
+		
+	];
+	this.midpointNew = [ // The midpoint of the rotated shape
+		(this.x + this.abNew[0]) / 2,
+		(this.y + this.abNew[1]) / 2
+	];
 	this.update = function() {
 		ctx = gameArea.context;
-		ctx.translate(x, y);
+
+		ctx.save();
 		ctx.rotate(this.rotation);
-		ctx.drawImage(this.image, this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
-		ctx.rotate(-this.rotation);
-		ctx.translate(-x, -y);
+		ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+		ctx.restore();
+
+		ctx.beginPath();
+		ctx.fillRect(this.abNew[0], this.abNew[1], 2, 2);
+		ctx.fillStyle = "red";
+		ctx.fill();
 	}
 }
 
