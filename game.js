763069fb3ed8b819245e261dae6297af065894d9
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-03-27 09:16:16 -0700

    Update size of blue ship

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ startGame = function() {
 }
 loadComponents = function() {
 	planet = new component(600, 600, 480, 540, 0, 1, "bluePlanet.png", true);
-	ship = new component(30, 30, 480, 100, 0, 1, "blueShip.png", false);
+	ship = new component(64, 64, 480, 100, 0, 1, "blueShip.png", false);
 	if (config.shaders == true) {
 		backgroundShade = new component(960, 540, 480, 270, 0, config.backgroundShadeAmount, "backgroundShade.png", false);
 		planetShade = new component(600, 600, 480, 540, 0, config.planetShadeAmount, "planetShade.png", false);	
