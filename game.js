f97fb96a5475e1241f158d53b3b865955960d73b
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-03-30 12:43:42 -0700

    Revamp code to favor future expansion

--- game.js
+++ game.js
@@ -1,26 +1,25 @@
 startGame = function() {
 	gameArea.start();
+	load = [];
 	loadComponents();
 }
 
 loadComponents = function() {
-	planet = new component(600, 600, 480, 540, 0, 1, "bluePlanet.png");
-	ship = new component(64, 64, 480, 100, 0, 1, "blueShip.png");
-	background = new component(1450, 1450, 480, 540, 0, 1, "background.png")
-	colonizeButton = new component(128, 64, 64, 32, 0, 1, "colonizeButton.png");
-	placehold1 = new component(128, 64, 64, 96 + 4, 0, 1, "placeholderButton.png");
-	placehold2 = new component(128, 64, 192 + 4, 32, 0, 1, "placeholderButton.png");
-	if (config.shaders == true) {
-		backgroundShade = new component(960, 540, 480, 270, 0, config.backgroundShadeAmount, "backShade.png");
-		planetShade = new component(600, 600, 480, 540, 0, config.planetShadeAmount, "planetShade.png");	
-	}
+	background = new planet(1450, 1450, 480, 540, "background.png");
+	backgroundShade = new shadow(960, 540, 480, 270, "backShade.png", config.backgroundShadeAmount);
+	planet = new planet(600, 600, 480, 540, "bluePlanet.png");
+	planetShade = new shadow(600, 600, 480, 540, "planetShade.png", config.planetShadeAmount);
+	ship = new shipPart(64, 64, 480, 100, "blueShip.png");
+	colonizeButton = new button(128, 64, 64, 32, "colonizeButton.png");
+	placehold1 = new button(128, 64, 64, 96 + 4, "placeholderButton.png");
+	placehold2 = new button(128, 64, 192 + 4, 32, "placeholderButton.png");	
 }
 
 config = {
 	updatePeriod : 20, // Lower: more screen updates per second. Keep this at 20.
 	shaders : true, // Purely visual. Adds shadows everywhere.
 	planetShadeAmount : 0.3, // Transparency of the planet shadow. Values range from 1 (dark) to 0 (no shadow).
-	backgroundShadeAmount : 0.9, // Transparency of the background shadow. Values range from 1 (dark) to 0 (no shadow).
+	backgroundShadeAmount : 0.3, // Transparency of the background shadow. Values range from 1 (dark) to 0 (no shadow).
 	rotateLeftKey : 68, // Key used to rotate things to the left.
 	rotateRightKey : 65, // Key used to rotate things to the right.
 	thrustKey : 87, // Key used to turn the ship's thrusters on. 
@@ -48,60 +47,33 @@ gameArea = {
 	}
 }
 
-function button( // Create buttons to display on the screen
-	width, // Width of the image to draw in pixels.
-	height, // Height of the image to draw in pixels.
-	x, // X coordinate of the midpoint of the image in pixels. 
-	y, // Y coordinate of the midpoint of the image in pixels.
-	source // URL of the image.
-	) {
-
-}
-
-function component(
-	width, // Width of the image to draw in pixels.
-	height, // Height of the image to draw in pixels.
-	x, // X coordinate of the midpoint of the image in pixels. 
-	y, // Y coordinate of the midpoint of the image in pixels.
-	rotation, // Default rotation of an image.
-	transparency, // Transparency of the image. Ranges from 1 (no transparency) to 0 (see-through).
-	source // URL of the image.
-	) {
+function button(width, height, x, y, source) {
+	load.push(this);
 	this.image = new Image();
 	this.image.src = source;
 	this.width = width;
 	this.height = height;
 	this.x = x - this.width / 2;
 	this.y = y - this.height / 2;
-	this.rotation = rotation * Math.PI / 180;
-	this.transparency = transparency;
-	this.speed = 0;
-	this.update = function() { // Draws the component with the correct rotation. Must be called after other 'update' type functions.
-		this.ab = [ // The bottom right corner of a shape at 0 degrees rotation
-			this.width + this.x,
-			this.height + this.y
-		];
-		this.midpoint = [ // The midpoint of a shape at 0 degrees rotation
-			(this.x + this.ab[0]) / 2, 
-			(this.y + this.ab[1]) / 2
-		];
-		this.midpointNew = [ // The midpoint of the rotated shape
-			this.midpoint[1] * Math.sin(-this.rotation) + this.midpoint[0] * Math.cos(-this.rotation),
-			this.midpoint[1] * Math.cos(-this.rotation) - this.midpoint[0] * Math.sin(-this.rotation)
-		];
-		this.midpointDiff = [ // The difference between the two midpoints
-			this.midpoint[0] - this.midpointNew[0],
-			this.midpoint[1] - this.midpointNew[1]
-		];
+	this.update = function() {
 		ctx = gameArea.context;
 		ctx.save();
-		ctx.translate(this.midpointDiff[0], this.midpointDiff[1]);
-		ctx.rotate(this.rotation);
-		ctx.globalAlpha = this.transparency;
 		ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
 		ctx.restore();
 	}
-	this.updateRotation = function() { // Updates the rotation of a (planet) shape based on keys the user has pressed.
+}
+
+function planet(width, height, x, y, source) {
+	load.push(this);
+	this.image = new Image();
+	this.image.src = source;
+	this.width = width;
+	this.height = height;
+	this.x = x - this.width / 2;
+	this.y = y - this.height / 2;
+	this.rotation = 0;
+	this.speed = 0;
+	this.update = function() {
 		if (gameArea.keys && gameArea.keys[config.thrustKey] && ship.rotation >= 5 * Math.PI / 180) {
 			this.speed -= (this.speed + 0.1) * (1 * Math.PI / 180); 
 		}
@@ -115,8 +87,38 @@ function component(
 			this.speed = -1 * Math.PI / 180;
 		}
 		this.rotation += this.speed;
+		this.midpoint = [
+			(this.x + (this.width + this.x)) / 2, 
+			(this.y + (this.height + this.y)) / 2
+		];
+		this.midpointNew = [
+			this.midpoint[1] * Math.sin(-this.rotation) + this.midpoint[0] * Math.cos(-this.rotation),
+			this.midpoint[1] * Math.cos(-this.rotation) - this.midpoint[0] * Math.sin(-this.rotation)
+		];
+		this.midpointDiff = [
+			this.midpoint[0] - this.midpointNew[0],
+			this.midpoint[1] - this.midpointNew[1]
+		];
+		ctx = gameArea.context;
+		ctx.save();
+		ctx.translate(this.midpointDiff[0], this.midpointDiff[1]);
+		ctx.rotate(this.rotation);
+		ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+		ctx.restore();
 	}
-	this.updateShipRotation = function() { // Updates the rotation of a (ship) shape based on keys the user has pressed.
+}
+
+function shipPart(width, height, x, y, source) {
+	load.push(this);
+	this.image = new Image();
+	this.image.src = source;
+	this.width = width;
+	this.height = height;
+	this.x = x - this.width / 2;
+	this.y = y - this.height / 2;
+	this.rotation = 0;
+	this.speed = 0;
+	this.update = function() {
 		if (this.rotation > 180 * Math.PI / 180) { // Keeps the rotation within the right bounds, so the following statements work at any rotation.
 			this.rotation -= 360 * Math.PI / 180;
 		} else if (this.rotation < -180 * Math.PI / 180) {
@@ -148,32 +150,46 @@ function component(
 			this.speed = -2 * Math.PI / 180;
 		}
 		this.rotation += this.speed;
+		this.midpoint = [
+			(this.x + (this.width + this.x)) / 2, 
+			(this.y + (this.height + this.y)) / 2
+		];
+		this.midpointNew = [
+			this.midpoint[1] * Math.sin(-this.rotation) + this.midpoint[0] * Math.cos(-this.rotation),
+			this.midpoint[1] * Math.cos(-this.rotation) - this.midpoint[0] * Math.sin(-this.rotation)
+		];
+		this.midpointDiff = [
+			this.midpoint[0] - this.midpointNew[0],
+			this.midpoint[1] - this.midpointNew[1]
+		];
+		ctx = gameArea.context;
+		ctx.save();
+		ctx.translate(this.midpointDiff[0], this.midpointDiff[1]);
+		ctx.rotate(this.rotation);
+		ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+		ctx.restore();
 	}
+}
+
+function shadow(width, height, x, y, source, transparency) {
+	load.push(this);
+	this.image = new Image();
+	this.image.src = source;
+	this.width = width;
+	this.height = height;
+	this.x = x - this.width / 2;
+	this.y = y - this.height / 2;
+	this.transparency = transparency;
+	this.update = function() {
+		ctx = gameArea.context;
+		ctx.save();
+		ctx.globalAlpha = this.transparency;
+		ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+		ctx.restore();
+	}	
 }
 
 function updateGameArea() { // Order to call functions: updateRotation/updateShipRotation/update
 	gameArea.clear();
-	if (config.shaders == true) { // Shadows should be drawn directly after the update() is called for what they are shadowing
-		background.updateRotation();
-		planet.updateRotation();
-		ship.updateShipRotation();
-		background.update();
-		backgroundShade.update();
-		planet.update();
-		planetShade.update();
-		ship.update();
-		colonizeButton.update();
-		placehold1.update();
-		placehold2.update();
-	} else {
-		background.updateRotation();
-		planet.updateRotation();
-		ship.updateShipRotation();
-		background.update();
-		planet.update();
-		ship.update();
-		colonizeButton.update();
-		placehold1.update();
-		placehold2.update();
-	}
+	for (var i = 0; i < load.length; i++) {load[i].update();}
 }
